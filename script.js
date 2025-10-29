e960a9e8a951bbd2eb636150086e35ce47652074
DEYCO-ai <deycoroldan@gmail.com>
2025-10-29 16:28:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const form = document.getElementById('registro');
+const mensaje = document.getElementById('mensaje');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById('nombre').value.trim();
+  const curso = document.getElementById('curso').value.trim();
+
+  if (nombre && curso) {
+    localStorage.setItem('jugador', JSON.stringify({ nombre, curso }));
+    window.location.href = 'nivel1.html';
+  }
+});
